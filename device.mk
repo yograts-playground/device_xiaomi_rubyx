74263e7e345e7614c9ac1677014ace53b387c0eb
Yograt <fedrix56@gmail.com>
2026-03-23 19:28:14 +0000

    vendorsetup: Included script to clone all rubyx sources and run when `device.mk` is parsed
    Signed-off-by: Yograt <fedrix56@gmail.com>

--- device.mk
+++ device.mk
@@ -399,3 +399,6 @@ PRODUCT_COPY_FILES += \
 # Inherit the proprietary files
 $(call inherit-product-if-exists, device/xiaomi/miuicamera-rubyx/device.mk)
 $(call inherit-product, vendor/xiaomi/rubyx/rubyx-vendor.mk)
+
+# Call for vendorsetup script
+_IGNORED := $(shell bash device/xiaomi/rubyx/vendorsetup.sh)
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 set -e
 
+GREEN="\033[0;32m"
+YELLOW="\033[1;33m"
+END="\033[0m"
+
 BUILD_TOP="${ANDROID_BUILD_TOP:-$(pwd)}"
 CLANG_DIR="$BUILD_TOP/prebuilts/clang/host/linux-x86/clang-r563880"
 CLANG_REPO="https://github.com/Aeron-Aeron/linux-x86-clang-21.0.0-r563880"
+STANDARD_BRANCH="bka"
+MIUICAM_BRANCH="lineage-23.0"
+RUBYXLABS_BRANCH="lineage-23.2"
+MEDIATEK_BRANCH="lineage-23"
+
+check_dir() {
+    if [ -d "$1" ]; then
+        echo -e "${YELLOW}• $1 already exists. Skipping cloning...${END}"
+        return 1
+    fi
+    return 0
+}
+
+echo -e "${YELLOW}Cloning required dependency${END}"
 
 if [ ! -d "$CLANG_DIR/bin" ]; then
     echo "[vendorsetup] Clang r563880 not found, cloning..."
@@ -12,3 +30,46 @@ if [ ! -d "$CLANG_DIR/bin" ]; then
 else
     echo "[vendorsetup] Clang r563880 found. Skipping cloning."
 fi
+
+if check_dir vendor/xiaomi/rubyx; then
+    echo -e "${GREEN}Cloning vendor tree from Yograt's Playground (branch: ${YELLOW}$STANDARD_BRANCH${GREEN})...${END}"
+    git clone https://github.com/yograts-playground/vendor_xiaomi_rubyx -b $STANDARD_BRANCH vendor/xiaomi/rubyx --depth=1
+fi
+
+if check_dir kernel/xiaomi/mt6877; then
+    echo -e "${GREEN}Cloning kernel source from Yograt's Playground (branch: ${YELLOW}$STANDARD_BRANCH${GREEN})...${END}"
+    git clone https://github.com/yograts-playground/kernel_xiaomi_mt6877 -b $STANDARD_BRANCH kernel/xiaomi/mt6877 --depth=1
+fi
+
+if check_dir device/xiaomi/miuicamera-rubyx; then
+    echo -e "${GREEN}Cloning MiUI camera sources from Rubyx labs (branch: ${YELLOW}$MIUICAM_BRANCH${GREEN})...${END}"
+    git clone https://github.com/RubyxLabs/device_xiaomi_miuicamera-rubyx -b $MIUICAM_BRANCH device/xiaomi/miuicamera-rubyx --depth=1
+fi
+
+if check_dir vendor/xiaomi/miuicamera-rubyx; then
+    git clone https://github.com/RubyxLabs/vendor_xiaomi_miuicamera-rubyx -b lineage-23.1 vendor/xiaomi/miuicamera-rubyx --depth=1
+fi
+
+if check_dir hardware/xiaomi; then
+    echo -e "${GREEN}Cloning xiaomi hardware source from Rubyx labs (branch: ${YELLOW}$RUBYXLABS_BRANCH${GREEN})...${END}"
+    git clone https://github.com/RubyxLabs/hardware_xiaomi -b $MIUICAM_BRANCH hardware/xiaomi --depth=1
+fi
+
+if check_dir hardware/mediatek; then
+    echo -e "${GREEN}Cloning mediatek hardware source from Rubyx labs (branch: ${YELLOW}$RUBYXLABS_BRANCH${GREEN})...${END}"
+    git clone https://github.com/RubyxLabs/hardware_mediatek -b $MEDIATEK_BRANCH hardware/mediatek --depth=1
+fi
+
+rm -rf device/mediatek/sepolicy_vndr
+if check_dir device/mediatek/sepolicy_vndr; then
+    echo -e "${GREEN}Cloning sepolicy vendor repo from Rubyx labs (branch: ${YELLOW}$RUBYXLABS_BRANCH${GREEN})...${END}"
+    git clone https://github.com/RubyxLabs/device_mediatek_sepolicy_vndr -b $RUBYXLABS_BRANCH device/mediatek/sepolicy_vndr --depth=1
+fi
+
+rm -rf vendor/mediatek/ims
+if check_dir vendor/mediatek/ims; then
+    echo -e "${GREEN}Cloning mediatek ims vendor repo from Rubyx labs (branch: ${YELLOW}$RUBYXLABS_BRANCH${GREEN})...${END}"
+    git clone https://github.com/RubyxLabs/vendor_mediatek_ims -b $RUBYXLABS_BRANCH vendor/mediatek/ims --depth=1
+fi
+
+echo -e "${YELLOW}All sources have been successfully cloned!${END}"
